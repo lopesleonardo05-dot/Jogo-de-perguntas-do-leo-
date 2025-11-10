a6a2119fda98f492c7961b2770de3ffbde299108
lopesleonardo05-dot <lopes.leonardo05@escola.pr.gov.br>
2025-11-10 10:05:14 -0300

    Update Jogo.py
    Após uma longa viajem você sofre um acidente (uma galinha lhê picou) e você está condenado(a) a responder perguntas de matemática (e outros não importantes) por motivos de roteiro

--- Jogo.py
+++ Jogo.py
@@ -1,8 +1,19 @@
 print ("Carregando...")
+print("Jogo daora carregado.")
+print("Após uma longa viajem você sofre um acidente (uma galinha lhê picou) e você está condenado(a) a responder perguntas de matemática (e outros não importantes) por motivos de roteiro, você respondera a seguinte questão:")
+import random
 
-#Descrição ,':]
-print("Jogo daora")
-#Pergunta :D
-print("1 + 2")
-resposta = input("Qual a resposta? ")
-if resposta == 3: print("Correto!")
+print("Qual o animal mais pesado do planeta?")
+resposta1 = input("Qual a resposta?")
+if resposta1 == ("baleia azul"):
+    print("Acertou")
+else:
+    print("errou... Era uma pergunta dificil mesmo.")
+a = random.randint (10,100)
+b = random.randint (10,100)
+print(a, "+", b)
+resposta2 = int(input("Qual a resposta?"))
+if resposta2 == a + b:
+    print("Acertou, não fez mais do que era seu dever, com uma pergunta facil dessas eu também conseguiria.")
+else:
+    print("ERROU KKKKKKK NUMA PERGUNTA FACIL DESSA? KKKKKKKK, a resposta era", a + b)
