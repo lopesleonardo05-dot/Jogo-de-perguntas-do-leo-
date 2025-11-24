a0c372957d78899d04b1d22094e07fecc3f2bf70
lopesleonardo05-dot <lopes.leonardo05@escola.pr.gov.br>
2025-11-24 09:05:01 -0300

    Update Jogo.py

--- Jogo.py
+++ Jogo.py
@@ -8,24 +8,31 @@ resposta1 = input(" 1. Qual a resposta?")
 if resposta1 == ("baleia azul"):
     print("Acertou")
 else:
-    print("errou... Era uma pergunta dificil mesmo.")
+    print("errou... Era uma pergunta dificil mesmo, era a baleia azul.")
 print("Qual é o animal mais rápido do planeta?")
 resposta2 = input("2. Qual a resposta?")
 if resposta2 == ("falcao peregrino"):
     print("Acertou")
 else:
-    print("errou... Era uma pergunta dificil mesmo.")
-
-
-
-
-
-
+    print("errou... Era uma pergunta dificil mesmo, era o falcao peregrino.")
 a = random.randint (10,100)
 b = random.randint (10,100)
 print(a, "+", b)
 resposta3 = int(input("3. Qual a resposta?"))
 if resposta3 == a + b:
-    print("Acertou, não fez mais do que era seu dever, com uma pergunta facil dessas eu também conseguiria.")
+    print("Acertou...")
 else:
-    print("ERROU KKKKKKK NUMA PERGUNTA FACIL DESSA? KKKKKKKK, a resposta era", a + b)
+    print("ERROU era", a + b)
+    print("Qual é a resposta da questão: 15 + 90 = ?")
+resposta4 = input("4. Qual a resposta?")
+if resposta4 == ("105"):
+    print("Acertou")
+else:
+    print("errou... Era uma pergunta dificil mesmo, era 105.")
+    print("Qual o animal mais forte?")
+resposta5 = input("5. Qual a resposta?")
+if resposta5 == ("besouro rinocerounte"):
+    print("Acertou")
+else:
+    print("errou... Era uma pergunta dificil mesmo, era o besouro rinocerounte.")
+    
